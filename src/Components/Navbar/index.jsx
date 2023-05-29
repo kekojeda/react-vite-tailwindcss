44aa487a5b2d7ab746a292fd733d91e721effe72
Sergio Ojeda <sergio.ojeda@vstudios.local>
2023-05-29 16:31:39 -0300

    navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+function NavBar(){
+    return (
+        <nav>
+            <ul>
+                <li>
+                    Home
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export { Navbar }
